84ea6cf812a5f45b6c4f6cbbf6d891d09b5d084a
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2014-02-27 17:53:09 +0100

    Initial work to get a python interface, non working so far

---
+++ pyPTAM/src/pyPTAM.cpp
@@ -0,0 +1,43 @@
+// This is a wrapper to the PTAM start procedure, exposing it to python using Boost.Python
+// Very basic from now, just testing the idea..
+// Benjamin Lefaudeux
+
+#include <stdlib.h>
+#include <iostream>
+#include <gvars3/instances.h>
+#include "System.h"
+
+#include <boost/python.hpp>
+
+using namespace std;
+using namespace GVars3;
+
+int instanciate()
+{
+  cout << " Starting the PTAM Slam " << endl;
+  cout << "  Parsing settings.cfg ...." << endl;
+
+  GUI.LoadFile("settings.cfg");
+  GUI.StartParserThread(); // Start parsing of the console input
+  atexit(GUI.StopParserThread);
+
+  try
+    {
+      System s;
+      s.Run();
+    }
+  catch(CVD::Exceptions::All e)
+    {
+      cout << endl;
+      cout << "!! Failed to run system; got exception. " << endl;
+      cout << "   Exception was: " << endl;
+      cout << e.what << endl;
+    }
+}
+
+
+BOOST_PYTHON_MODULE(pyPTAM)
+{
+    using namespace boost::python;
+    def("instanciate", instanciate);
+}
