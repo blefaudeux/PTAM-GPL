6b338bd37557dd8c40bedbdf745f6cf12f48cfaa
Benjamin Lefaudeux <blefaudeux@github>
2014-03-21 16:59:51 +0100

    Initial commit

--- libPTAM/external_libs/gvars/gvars3/config.h
+++ libPTAM/external_libs/gvars/gvars3/config.h
@@ -1,5 +1,5 @@
 /* gvars3/config.h.  Generated from config.h.in by configure.  */
 #ifndef GVARS3_INCLUDE_CONFIG_H
 #define GVARS3_INCLUDE_CONFIG_H
-/* #undef GVARS3_HAVE_TOON */
+#define GVARS3_HAVE_TOON 1
 #endif
