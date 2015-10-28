92353da862705a8a0a6f632a623f161bfa91f20a
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-10-28 19:29:19 +0100

    now dealing with the dependencies building at cmake time, nice

--- install_scripts/cvd.sh
+++ install_scripts/cvd.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir $2/lib
-mkdir $2/include
+mkdir $2/cvd
 
 cp $1/libcvd* $2/lib
-cp $1/cvd/* $2/include
+cp $1/cvd/* $2/cvd
 
 echo "CVD locally installed"
--- install_scripts/gvars.sh
+++ install_scripts/gvars.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir $2/lib
-mkdir $2/include
+mkdir $2/gvars3
 
 cp $1/libGVars* $2/lib
-cp $1/gvars3/* $2/include
+cp $1/gvars3/* $2/gvars3
 
 echo "GVars3 locally installed"
