212cff8f83edf16790c877c3346b7a2874ea98f4
Benjamin Lefaudeux <benjamin.lefaudeux@gmail.com>
2015-10-30 17:33:03 +0100

    Fixing build issues

--- install_scripts/cvd.sh
+++ install_scripts/cvd.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 mkdir $2/lib
 mkdir $2/cvd
+mkdir $2/cvd/internal
+mkdir $2/cvd/Linux
+
 
 cp $1/libcvd* $2/lib
 cp $1/cvd/* $2/cvd
+cp $1/cvd/internal/* $2/cvd/internal/.
+cp $1/cvd/Linux/* $2/cvd/Linux/.
 
 echo "CVD locally installed"
